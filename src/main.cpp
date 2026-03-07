49a8f5f54170a5e0082f96f2ab7cf4d5d472de5c
Bautista Rodriguez <bautirodri@gmail.com>
2026-03-07 17:45:02 +0100

    chore: add main for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
 #include "Server.hpp"
 
-int main(void) {
-  Server serv;
+// int main(void) {
+//   Server serv;
+//
+//   serv.run();
+//   return (0);
+// }
 
-  serv.run();
-  return (0);
+// hasta que tengamos el ProcessManager uwu
+#include "ConfigParser.hpp"
+#include "Program.hpp"
+#include <vector>
+
+int main() {
+  ConfigParser parser;
+
+  std::map<std::string, ProgramConfig> config_map = parser.parse("config.lua");
+
+  std::vector<Program *> programs;
+
+  for (const auto &pair : config_map) {
+    Program *prog = new Program(pair.first, pair.second);
+    programs.push_back(prog);
+
+    if (prog->getConfig().autostart) {
+      prog->start();
+    }
+  }
+
+  for (Program *prog : programs) {
+    delete prog;
+  }
+
+  return 0;
 }
