d7d1fe1ff683d464a42f6d05cf568470700e4c06
Bautista Rodriguez <bautirodri@gmail.com>
2026-03-07 18:58:10 +0100

    chore: add Process test in main

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,40 @@
 // hasta que tengamos el ProcessManager uwu
 #include "ConfigParser.hpp"
 #include "Program.hpp"
+#include <sys/wait.h>
+#include <unistd.h>
 #include <vector>
 
 int main() {
   ConfigParser parser;
-
-  std::map<std::string, ProgramConfig> config_map = parser.parse("config.lua");
-
+  auto config_map = parser.parse("config.lua");
   std::vector<Program *> programs;
 
-  for (const auto &pair : config_map) {
-    Program *prog = new Program(pair.first, pair.second);
+  for (const auto &[name, cfg] : config_map) {
+    Program *prog = new Program(name, cfg);
     programs.push_back(prog);
 
-    if (prog->getConfig().autostart) {
+    if (cfg.autostart) {
       prog->start();
     }
   }
+
+  std::cout << "\n--- waiting for children ---\n";
+
+  // algo asi tendria que hacer el ProcessManager pero mejor xd
+  int status;
+  pid_t wpid;
+  while ((wpid = wait(&status)) > 0) {
+    if (WIFEXITED(status)) {
+      std::cout << "[Test] PID " << wpid << " finished with exit code "
+                << WEXITSTATUS(status) << "\n";
+    } else if (WIFSIGNALED(status)) {
+      std::cout << "[Test] PID " << wpid << " was killed by signal "
+                << WTERMSIG(status) << "\n";
+    }
+  }
+
+  std::cout << "\n--- finished ---\n";
 
   for (Program *prog : programs) {
     delete prog;
