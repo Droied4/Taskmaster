26a6df23cbb5b2d72f9b8501bdd64206d00d5f6b
Bautista Rodriguez <bautirodri@gmail.com>
2026-03-09 13:35:59 +0100

    style: formatting

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -1,44 +1,45 @@
 #ifndef SERVER_HPP
-# define SERVER_HPP
+#define SERVER_HPP
 
-# include <iostream>
-# include <vector>
-# include <memory>
-# include <sys/socket.h>
-# include <fcntl.h>
-# include <cstring>
-# include <sys/un.h>
-# include <sys/epoll.h>
-# include <strings.h>
-# include "Logs.hpp"
+#include "Logs.hpp"
+#include "common.hpp"
+#include <cstring>
+#include <fcntl.h>
+#include <iostream>
+#include <memory>
+#include <strings.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <vector>
 
-//no he testeado nada pero igual no esta acabado confia tuta
+// no he testeado nada pero igual no esta acabado confia tuta
 
-class Server{
-	private:
-		int 				_epfd;
-		int 				_serv_fd;
-		struct sockaddr_un 	_serv_addr;
+class Server {
+private:
+  int _epfd;
+  int _serv_fd;
+  struct sockaddr_un _serv_addr;
 
-		void setServerFd(int fd);
+  void setServerFd(int fd);
 
-	public:
-		static constexpr char 	SOCK_PATH[22] = "/tmp/taskmaster.sock";
-		static constexpr int 	BUFFER_SIZE = 1024;
-		static constexpr int 	EVENTS_SIZE = 64;
+public:
+  static constexpr char SOCK_PATH[22] = "/tmp/taskmaster.sock";
+  static constexpr int BUFFER_SIZE = 1024;
+  static constexpr int EVENTS_SIZE = 64;
 
-		Server();
-		Server(const Server &obj);
-		Server &operator=(const Server &obj);
-		~Server();
+  Server();
+  Server(const Server &obj);
+  Server &operator=(const Server &obj);
+  ~Server();
 
-		void bindListen();
-		void run();
-		void acceptConnection(); 
-		void readData(int fd);
-		void sendData();
+  void bindListen();
+  void run();
+  void acceptConnection();
+  void readData(int fd);
+  void sendData();
 
-		int getServerFd() const;	
+  int getServerFd() const;
 };
 
-#endif 
+#endif
