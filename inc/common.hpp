34aef8380dbc7cd9468b514d77419730efb92b98
Bautista Rodriguez <bautirodri@gmail.com>
2026-03-09 16:41:35 +0100

    refactor: add Logs to macros

--- inc/common.hpp
+++ inc/common.hpp
@@ -1,20 +1,20 @@
 #define CONFIG_ERROR(prog, msg)                                                \
   do {                                                                         \
-    std::cerr << "Config Error [" << prog << "]: " << msg << "\n";             \
+    Logs::error() << "Config Error [" << prog << "]: " << msg << "\n";         \
     std::exit(1);                                                              \
   } while (0)
 
 #define ERROR(msg)                                                             \
   do {                                                                         \
     Logs::error() << msg << std::endl;                                         \
-    exit(1);                                                                   \
+    std::abort();                                                              \
   } while (0)
 
 #define ASSERT(condition, message)                                             \
   do {                                                                         \
     if (!(condition)) {                                                        \
-      std::cerr << "SYSTEM ASSERT FAILED: " << message << "\n"                 \
-                << "File: " << __FILE__ << " Line: " << __LINE__ << "\n";      \
+      Logs::error() << "SYSTEM ASSERT FAILED: " << message << "\n"             \
+                    << "File: " << __FILE__ << " Line: " << __LINE__ << "\n";  \
       std::abort();                                                            \
     }                                                                          \
   } while (0)
