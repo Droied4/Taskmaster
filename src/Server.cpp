9a9065240c9bf0e802c2166bc7b225cc7790aa8a
Deivid Santiago Ordoñez Varela <deordone@cbr2s4.42barcelona.com>
2026-03-09 16:10:44 +0100

    refactor: add assert to server class

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,24 +1,26 @@
 #include "Server.hpp"
 
-Server::Server() {
-  this->_serv_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-  if (this->_serv_fd < 0)
-    ERROR("Sever fd socket failed!");
-  Logs::info() << "Server Socket Created\n";
-  Logs::debug() << "Epoll Event created with server fd: " << this->_serv_fd
-                << "\n";
-  bzero(&this->_serv_addr, sizeof(this->_serv_addr));
-  this->_serv_addr.sun_family = AF_UNIX;
-  strncpy(this->_serv_addr.sun_path, this->SOCK_PATH,
-          sizeof(this->_serv_addr.sun_path) - 1);
-  this->_serv_addr.sun_path[sizeof(this->_serv_addr.sun_path) - 1] = '\0';
+Server::Server() 
+{
+	ASSERT(strlen(SOCK_PATH) > 0, "SOCK_PATH must be declared");
+	this->_serv_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+	if (this->_serv_fd < 0)
+		ERROR("Sever fd socket failed!");
+	Logs::info() << "Server Socket Created\n";
+	Logs::debug() << "Epoll Event created with server fd: " << this->_serv_fd
+		<< "\n";
+	bzero(&this->_serv_addr, sizeof(this->_serv_addr));
+	this->_serv_addr.sun_family = AF_UNIX;
+	strncpy(this->_serv_addr.sun_path, this->SOCK_PATH,
+			sizeof(this->_serv_addr.sun_path) - 1);
+	this->_serv_addr.sun_path[sizeof(this->_serv_addr.sun_path) - 1] = '\0';
 
-  this->_epfd = epoll_create1(0);
-  struct epoll_event ev;
-  ev.events = EPOLLIN | EPOLLRDHUP;
-  ev.data.fd = this->_serv_fd;
+	this->_epfd = epoll_create1(0);
+	struct epoll_event ev;
+	ev.events = EPOLLIN | EPOLLRDHUP;
+	ev.data.fd = this->_serv_fd;
 
-  epoll_ctl(this->_epfd, EPOLL_CTL_ADD, this->_serv_fd, &ev);
+	epoll_ctl(this->_epfd, EPOLL_CTL_ADD, this->_serv_fd, &ev);
 }
 
 Server::Server(const Server &obj) { *this = obj; }
@@ -36,25 +38,26 @@ void Server::setServerFd(int fd) { this->_serv_fd = fd; }
 int Server::getServerFd() const { return (this->_serv_fd); }
 
 void Server::bindListen() {
-  unlink(SOCK_PATH);
-  Logs::debug() << "Unlinked Sock Path: " << SOCK_PATH << "\n";
-  if (bind(this->_serv_fd, reinterpret_cast<sockaddr *>(&this->_serv_addr),
-           sizeof(this->_serv_addr)) < 0)
-    ERROR("Bind failed!\n");
-  Logs::info() << "Server binded on route: " << SOCK_PATH << "\n";
-  if (listen(this->_serv_fd, 0) < 0)
-    ERROR("Server listen failed!\n");
-  Logs::info() << "Server is now Listening!\n";
+	unlink(SOCK_PATH);
+	Logs::debug() << "Unlinked Sock Path: " << SOCK_PATH << "\n";
+	if (bind(this->_serv_fd, reinterpret_cast<sockaddr *>(&this->_serv_addr),
+				sizeof(this->_serv_addr)) < 0)
+		ERROR("Bind failed!\n");
+	Logs::info() << "Server binded on route: " << SOCK_PATH << "\n";
+	if (listen(this->_serv_fd, 0) < 0)
+		ERROR("Server listen failed!\n");
+	Logs::info() << "Server is now Listening!\n";
 }
 
 void Server::readData(int fd) {
-  char buffer[BUFFER_SIZE + 1];
-  bzero(buffer, BUFFER_SIZE + 1);
-  int bytes;
+	ASSERT(BUFFER_SIZE > 0, "BUFFER_SIZE must be above 0");
+	char buffer[BUFFER_SIZE + 1];
+	bzero(buffer, BUFFER_SIZE + 1);
+	int bytes;
 
-  bytes = read(fd, buffer, BUFFER_SIZE);
-  if (bytes <= 0) {
-    Logs::info() << "Client Disconnected : " << fd << "\n";
+	bytes = read(fd, buffer, BUFFER_SIZE);
+	if (bytes <= 0) {
+		Logs::info() << "Client Disconnected : " << fd << "\n";
     epoll_ctl(this->_epfd, EPOLL_CTL_DEL, fd, nullptr);
     close(fd);
     return;
