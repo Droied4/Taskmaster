864cee9717d17d4889696581ef0886eabe48eb0a
Bautista Rodriguez <bautirodri@gmail.com>
2026-03-14 18:25:33 +0100

    style: formatting

--- inc/Daemon.hpp
+++ inc/Daemon.hpp
@@ -1,22 +1,27 @@
 #pragma once
 
-#include <iostream>
-#include "Server.hpp"
 #include "Logs.hpp"
+#include "ProcessManager.hpp"
+#include "Server.hpp"
+#include <iostream>
 #include <sys/epoll.h>
 
 class Daemon {
-	private:
-		int _epfd;
-		Server _serv;
-	
-	public:
-		Daemon();
-		Daemon(const Daemon& obj) = delete;
-		Daemon& operator=(const Daemon& obj) = delete;
-		~Daemon();
+private:
+  int _epfd;
+  int _sig_fd;
+  Server _serv;
+  ProcessManager _manager;
 
-  		static constexpr int EVENTS_SIZE = 64;
+  void setupSignals();
 
-  		void run();
+public:
+  Daemon();
+  Daemon(const Daemon &obj) = delete;
+  Daemon &operator=(const Daemon &obj) = delete;
+  ~Daemon();
+
+  static constexpr int EVENTS_SIZE = 64;
+
+  void run();
 };
