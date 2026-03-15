3e3839be7133b17efd80a1490692d2813ae61fd9
Bautista Rodriguez <bautirodri@gmail.com>
2026-03-15 16:47:31 +0100

    fix: wait for processes to stop before restarting

--- src/Program.cpp
+++ src/Program.cpp
@@ -2,6 +2,9 @@
 #include "Logs.hpp"
 #include "Process.hpp"
 #include "common.hpp"
+#include <cstdio>
+#include <sys/wait.h>
+#include <unistd.h>
 
 Program::Program(const std::string &name, const ProgramConfig &config)
     : _name(name), _config(config) {
@@ -10,8 +13,15 @@ Program::Program(const std::string &name, const ProgramConfig &config)
   ASSERT(_config.numprocs > 0, "Program must have at least 1 process");
 
   for (int i = 0; i < _config.numprocs; ++i) {
-    std::string proc_name = _name + "_" + std::to_string(i);
-    _processes.push_back(new Process(proc_name, _config));
+    std::string proc_name;
+    if (_config.numprocs == 1) {
+      proc_name = _name;
+    } else {
+      char suffix[16];
+      snprintf(suffix, sizeof(suffix), "_%02d", i);
+      proc_name = _name + suffix;
+    }
+    _processes.push_back(new Process(proc_name, _name, _config));
   }
 }
 
@@ -41,6 +51,43 @@ void Program::stop() {
 void Program::restart() {
   Logs::info() << "[Program] Restarting " << _name << "...\n";
   stop();
+
+  const int max_wait_ms = 10000;
+  const int poll_interval_us = 50000;
+  int waited_ms = 0;
+
+  while (waited_ms < max_wait_ms) {
+    bool all_stopped = true;
+    for (Process *p : _processes) {
+      ProcessState state = p->getState();
+      if (state != ProcessState::STOPPED && state != ProcessState::EXITED &&
+          state != ProcessState::FATAL) {
+        all_stopped = false;
+        break;
+      }
+    }
+
+    if (all_stopped) {
+      break;
+    }
+
+    int status;
+    pid_t reaped;
+    while ((reaped = waitpid(-1, &status, WNOHANG)) > 0) {
+      for (Process *p : _processes) {
+        if (p->getPid() == reaped) {
+          p->setState(ProcessState::STOPPED);
+          p->setEndTime(time(NULL));
+          p->setStatusMsg("Stopped");
+          break;
+        }
+      }
+    }
+
+    usleep(poll_interval_us);
+    waited_ms += poll_interval_us / 1000;
+  }
+
   start();
 }
 
