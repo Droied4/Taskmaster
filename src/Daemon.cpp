80e973ce1da4d7528c4f1056d084434942fe2554
Bautista Rodriguez <bautirodri@gmail.com>
2026-03-15 16:46:38 +0100

    fix: fix signal handling condition

--- src/Daemon.cpp
+++ src/Daemon.cpp
@@ -3,7 +3,9 @@
 #include <csignal>
 #include <sys/signalfd.h>
 
-Daemon::Daemon(ProcessManager &obj) : _epfd(epoll_create1(0)), _serv(_epfd), _manager(obj) {
+Daemon::Daemon(ProcessManager &obj)
+    : _epfd(epoll_create1(0)), _sig_fd(-1), _serv(_epfd), _manager(obj) {
+  ASSERT(_epfd >= 0, "Failed to create epoll instance");
 }
 
 Daemon::~Daemon() {}
@@ -31,8 +33,7 @@ void Daemon::setupSignals() {
   epoll_ctl(_epfd, EPOLL_CTL_ADD, _sig_fd, &ev);
 }
 
-void Daemon::run() 
-{
+void Daemon::run() {
   ASSERT(EVENTS_SIZE > 0, "EVENTS_SIZE must be above 0");
   struct epoll_event events[EVENTS_SIZE];
   setupSignals();
@@ -41,7 +42,7 @@ void Daemon::run()
   while (42) {
     int nfds = epoll_wait(_epfd, events, EVENTS_SIZE, 500);
     for (int i = 0; i < nfds; ++i) {
-  		int client_socket = events[i].data.fd;
+      int client_socket = events[i].data.fd;
       if (client_socket == _serv.getServerFd())
         _serv.acceptConnection(_epfd);
       else if (client_socket == _sig_fd) { // llega una nueva signal
@@ -49,7 +50,7 @@ void Daemon::run()
 
         ssize_t s = read(_sig_fd, &fdsi, sizeof(struct signalfd_siginfo));
 
-        if (s != sizeof(struct signalfd_siginfo)) {
+        if (s == sizeof(struct signalfd_siginfo)) {
           if (fdsi.ssi_signo == SIGHUP) {
             Logs::info()
                 << "[Daemon] Received SIGHUP, reloading configuration..."
@@ -59,17 +60,21 @@ void Daemon::run()
             Logs::info()
                 << "[Daemon] Received signal to terminate, shutting down..."
                 << std::endl;
-            // _manager.shutdownAll();
+            _manager.shutdownAll();
             return;
           }
         }
       } else {
-		std::string input = _serv.readData(client_socket,
-                       this->_epfd);
-		_cparser.setCommandParser(input);
-		std::string output = _manager.executeCommand(_cparser.getCommand(), _cparser.getParams());
-        _serv.sendData(client_socket, output);
+        std::string input = _serv.readData(client_socket, this->_epfd);
+        if (!input.empty()) {
+          _cparser.setCommandParser(input);
+          std::string output = _manager.executeCommand(_cparser.getCommand(),
+                                                       _cparser.getParams());
+          _serv.sendData(client_socket, output);
+        }
       }
     }
+    _manager.reap();
+    _manager.updateRunningStates();
   }
 }
