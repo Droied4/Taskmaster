567200f6dcdf57f4c3b4f7ef7f9003797dcf98b0
Bautista Rodriguez <bautirodri@gmail.com>
2026-03-16 22:07:59 +0100

    refactor: fixed bug when reloading config

--- inc/ConfigParser.hpp
+++ inc/ConfigParser.hpp
@@ -1,51 +1,53 @@
 #pragma once
 #include "Program.hpp"
+#include <functional>
 #include <iostream>
 #include <map>
 #include <string>
 #include <unordered_map>
 #include <vector>
-
 extern "C" {
 #include <lauxlib.h>
 #include <lua.h>
 #include <lualib.h>
 }
-
 class ConfigParser {
 private:
-  // es el estado de Lua que se usa para cargar la config
   lua_State *L;
-
-  using ParserAction = void (*)(ConfigParser *self,
-                                const std::string &prog_name,
-                                ProgramConfig &cfg);
-
-  void parseProgramTable(const std::string &prog_name, ProgramConfig &cfg);
-  void parseUmask(const std::string &prog_name, ProgramConfig &cfg);
-  void parseExitCodes(const std::string &prog_name, ProgramConfig &cfg);
-  void parseEnv(const std::string &prog_name, ProgramConfig &cfg);
-  void parseString(const std::string &key, const std::string &prog_name,
-                   std::string &out);
-  void parseInt(const std::string &key, const std::string &prog_name, int &out,
-                bool check_pos = false);
-  void parseBool(const std::string &key, const std::string &prog_name,
-                 bool &out);
-  void parseAutoRestart(const std::string &prog_name, ProgramConfig &cfg);
-  int getSignalFromLua(lua_State *L, const std::string &prog_name);
-
-  static const std::unordered_map<std::string, ParserAction> &getHandlers();
-
+  using ParserAction =
+      std::function<bool(ConfigParser *self, const std::string &prog_name,
+                         ProgramConfig &cfg, std::string &error)>;
+  bool parseProgramTable(const std::string &prog_name, ProgramConfig &cfg,
+                         std::string &error);
+  bool parseUmask(const std::string &prog_name, ProgramConfig &cfg,
+                  std::string &error);
+  bool parseExitCodes(const std::string &prog_name, ProgramConfig &cfg,
+                      std::string &error);
+  bool parseEnv(const std::string &prog_name, ProgramConfig &cfg,
+                std::string &error);
+  bool parseString(const std::string &key, const std::string &prog_name,
+                   std::string &out, std::string &error);
+  bool parseInt(const std::string &key, const std::string &prog_name, int &out,
+                std::string &error);
+  bool parseBool(const std::string &key, const std::string &prog_name,
+                 bool &out, std::string &error);
+  bool parseAutoRestart(const std::string &prog_name, ProgramConfig &cfg,
+                        std::string &error);
+  bool getSignalFromLua(lua_State *L, const std::string &prog_name, int &out,
+                        std::string &error);
+  bool parsePath(const std::string &key, const std::string &prog_name,
+                 std::string &out, std::string &error);
+  const std::unordered_map<std::string, ParserAction> &getHandlers();
 #ifdef DEBUG
   void
   debug_dump_config(const std::map<std::string, ProgramConfig> &configs) const;
   void dump_stack(lua_State *L);
 #endif
-
 public:
   ConfigParser();
   ~ConfigParser();
   ConfigParser(const ConfigParser &) = delete;
   ConfigParser &operator=(const ConfigParser &) = delete;
-  std::map<std::string, ProgramConfig> parse(const std::string &filename);
+  bool parse(const std::string &filename,
+             std::map<std::string, ProgramConfig> &out, std::string &error);
 };
