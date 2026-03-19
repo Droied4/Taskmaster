ce07bd0870d20ff440a6a117006ce9dc1c3b0df2
Deivid Santiago Ordoñez Varela <deordone@car11s3.42barcelona.com>
2026-03-20 00:45:31 +0100

    feat: help option

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,42 @@
 #include "Daemon.hpp"
 #include "Logs.hpp"
 #include "ProcessManager.hpp"
+#define COMMON_OPTSTR= ":?h"
+
+void help(void)
+{
+	std::cout << "Taskmasterd    \
+		-h display help\n 		 \
+		\n";
+}
+
+static void flagCases(int ac, char *av[])
+{
+	int ch;
+	opterr = 0;
+	while ((ch = getopt(ac, av, COMMON_OPTSTR)) != EOF) 
+	{
+		switch (ch)
+		{
+			case ':':
+				exit(1);
+				break ;
+			case '?':
+				help();
+				break ;
+			case 'h':
+				help();
+				break ;	
+		}
+	}
+}
 
 int main(int argc, char *argv[]) {
 #ifdef DEBUG
   Logs::setMinLevel(Logs::Level::LDEBUG);
 #endif
-
+	
+  flagCases(int ac, char **av);
   Logs::setFile("/home/droied/42/taskmaster/logs/logs.txt");
   std::string config_path = "config.lua";
   if (argc > 1) {
