aae567dddd0c1e01508ed26236ac034a0f40c006
Deivid Santiago Ordoñez Varela <deordone@car11s3.42barcelona.com>
2026-03-20 01:31:07 +0100

    feat: getopt_long

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,56 @@
 #include "Daemon.hpp"
 #include "Logs.hpp"
 #include "ProcessManager.hpp"
-#define COMMON_OPTSTR= ":?h"
+#include <getopt.h>
 
 void help(void)
 {
-	std::cout << "Taskmasterd    \
-		-h display help\n 		 \
+	std::cout << "--help \t\t-h -- print this ussage message and exit\n\
+		--configuration \t\t-c -- configuration file path\n\
+		--logfile \t\t-l -- logfile  path\n\
+		--loglevel \t\t-e -- log level\n\
+		--nodaemon \t\t-n -- run in the foreground\n\
+		--version \t\t-v -- print taskmasterd version number and exit\n\
 		\n";
 }
 
 static void flagCases(int ac, char *av[])
 {
-	int ch;
-	opterr = 0;
-	while ((ch = getopt(ac, av, COMMON_OPTSTR)) != EOF) 
-	{
-		switch (ch)
-		{
-			case ':':
-				exit(1);
-				break ;
-			case '?':
-				help();
-				break ;
+	std::vector<std::string> configs;
+
+	const char* short_opts = ":?h";
+
+	const option long_opts[] = {
+		{"help", no_argument, nullptr, 'h'},
+		{"configuration", required_argument, nullptr, 'c'},
+		{"logfile", required_argument, nullptr, 'l'},
+		{"loglevel", required_argument, nullptr, 'e'},
+		{"nodaemon", no_argument, nullptr, 'n'},
+		{"version", no_argument, nullptr, 'v'},
+		{nullptr, 0, nullptr, 0}
+	};
+
+	int opt;
+	int long_index = 0;
+
+	while ((opt = getopt_long(ac, av, short_opts, long_opts, &long_index)) != -1) {
+		switch (opt) {
 			case 'h':
 				help();
-				break ;	
+				exit(1);
+			default:
+				std::cerr << "Uso: " << argv[0] << " [-c config]\n";
+				return 1;
 		}
 	}
 }
 
-int main(int argc, char *argv[]) {
+int main(int ac, char *av[]) {
 #ifdef DEBUG
   Logs::setMinLevel(Logs::Level::LDEBUG);
 #endif
 	
-  flagCases(int ac, char **av);
+  flagCases(ac, av);
   Logs::setFile("/home/droied/42/taskmaster/logs/logs.txt");
   std::string config_path = "config.lua";
   if (argc > 1) {
