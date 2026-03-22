ec5595d2d47b4e02478e78854b64db928dc7098d
Bautista Rodriguez <bautirodri@gmail.com>
2026-03-22 01:42:41 +0100

    feat: add attach to process

--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -1,7 +1,10 @@
 #include "Client.hpp"
 #include <cstring>
+#include <iostream>
+#include <poll.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <termios.h>
 #include <unistd.h>
 
 Client::Client(const std::string &socket_path) : _socket_path(socket_path) {}
@@ -41,10 +44,81 @@ std::string Client::send(const std::string &command) {
 
   std::string response;
   char buffer[4096];
-  int bytes_read;
-  while ((bytes_read = recv(sock, buffer, sizeof(buffer) - 1, 0)) > 0) {
+
+  int bytes_read = recv(sock, buffer, sizeof(buffer) - 1, 0);
+
+  if (bytes_read > 0) {
     response.append(buffer, bytes_read);
+
+    if (response.find("ATTACH_OK") != std::string::npos) {
+      tv.tv_sec = 0;
+      tv.tv_usec = 0;
+      setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+
+      attachToProcess(sock);
+
+      close(sock);
+      return "";
+    }
+
+    while ((bytes_read = recv(sock, buffer, sizeof(buffer) - 1, 0)) > 0) {
+      response.append(buffer, bytes_read);
+    }
   }
+
   close(sock);
   return response;
+}
+void Client::attachToProcess(int socket_fd) {
+  struct termios oldt, newt;
+  tcgetattr(STDIN_FILENO, &oldt);
+
+  newt = oldt;
+
+  newt.c_lflag &= ~(ICANON | ECHO | ISIG);
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+  struct pollfd fds[2];
+  fds[0].fd = STDIN_FILENO;
+  fds[0].events = POLLIN;
+  fds[1].fd = socket_fd;
+  fds[1].events = POLLIN;
+
+  char buf[1024];
+
+  bool attached = true;
+
+  std::cout << "\r\n[Attached to process. Press Ctrl+Q to detach]\r\n";
+
+  while (attached) {
+    if (poll(fds, 2, -1) < 0)
+      break;
+
+    if (fds[0].revents & POLLIN) {
+      int n = read(STDIN_FILENO, buf, sizeof(buf));
+      if (n > 0) {
+        for (int i = 0; i < n; ++i) {
+          if (buf[i] == 3) { // Ctrl+C
+            attached = false;
+            break;
+          }
+        }
+        if (attached) {
+          write(socket_fd, buf, n);
+        }
+      }
+    }
+
+    if (fds[1].revents & POLLIN) {
+      int n = read(socket_fd, buf, sizeof(buf));
+      if (n <= 0) {
+        attached = false;
+      } else {
+        write(STDOUT_FILENO, buf, n);
+      }
+    }
+  }
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  std::cout << "\r\n[Detached from process]\r\n";
 }
