2258261a5539e5ed35b54f0cc3ddecca8b624524
Bautista Rodriguez <bautirodri@gmail.com>
2026-04-01 16:19:13 +0200

    chore: more tests

--- config.lua
+++ config.lua
@@ -21,23 +21,63 @@ config.programs = {}
 config.programs.cat = {
 	cmd = "cat",
 	autostart = true,
-	numprocs = 8,
-	stdout = "/tmp/cat.stdout",
-	stderr = "/tmp/cat.stderr",
+	numprocs = 1,
 }
 
 config.programs.echo = {
 	cmd = "echo 'Hello, World!'",
 	autostart = true,
+	stderr = "/tmp/echo.stderr",
+	stdout = "/tmp/echo.stdout",
 	numprocs = 8,
 }
 
 config.programs.ls = {
 	cmd = "ls",
 	autostart = true,
-	numprocs = 8,
+	autorestart = "always",
+	startretries = 5,
+	numprocs = 1,
 	stdout = "/tmp/ls.stdout",
 	stderr = "/tmp/ls.stderr",
+}
+
+config.programs.sleep = {
+	cmd = "sleep 10",
+	-- autostart = true,
+	numprocs = 3,
+	stoptime = 5,
+	stopsignal = "SIGTERM",
+}
+
+-- config.programs.test = {
+-- 	cmd = "./test.sh",
+-- 	autostart = true,
+-- 	stoptime = 4,
+-- 	stopsignal = "SIGTERM",
+-- }
+
+config.programs.bash = {
+	cmd = "bash",
+	autostart = false,
+	stopsignal = "SIGTERM",
+	env = {
+		TUQUI = "TUQUI",
+		TERM = "xterm",
+	},
+}
+
+config.programs.zsh = {
+	cmd = "zsh",
+	autostart = true,
+	autorestart = "always",
+	startretries = 5,
+	stoptime = 5,
+	stopsignal = "SIGQUIT",
+	env = {
+		TUQUI = "TUQUI",
+		TERM = "xterm",
+	},
 }
 
 return config
